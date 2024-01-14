4d45d65fc4b5023897cfaeb525fa37c2fbb0fd0d
AtomPariyakitti <pariyakittinakrub@gmail.com>
2024-01-14 23:02:18 +0700

    End of section 3

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,12 +1,20 @@
 
 import './styles.css'
+import EventDashboard from '../../features/events/dashboard/EventDashboard'
+import Navbar from './nav/Navbar'
+import { Container } from 'semantic-ui-react'
+import { useState } from 'react'
 
 function App() {
+  const [formOpen, setFormOpen] = useState(false)
 
   return (
-    <h1>
-      Welcome to Revents
-    </h1>
+    <>
+      <Navbar setFormOpen={setFormOpen} />
+      <Container className='main'>
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
+      </Container>
+    </>
   )
 }
 
