fdf865a0d96f6fd224db5f43cb4c8cf51e25454b
AtomPariyakitti <pariyakittinakrub@gmail.com>
2024-01-15 18:19:39 +0700

    End of section 4

--- src/features/events/dashboard/EventList.tsx
+++ src/features/events/dashboard/EventList.tsx
@@ -3,13 +3,20 @@ import EventListItem from "./EventListItem";
 
 type Props = {
   events: AppEvent[]
+  selectedEvent: (event: AppEvent) => void
+  deleteEvent: (event: string) => void
 }
 
-export default function EventList({events}: Props) {
+export default function EventList({events, selectedEvent, deleteEvent}: Props) {
   return (
     <>
       {events.map(event => (
-        <EventListItem key={event.id} event={event} />
+        <EventListItem 
+        key={event.id} 
+        event={event} 
+        selectEvent={selectedEvent} 
+        deleteEvent={deleteEvent}
+        />
       ))}
     </>
   )
